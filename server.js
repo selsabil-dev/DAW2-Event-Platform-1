76138b63220f321219d93db87f6a3facbb9771ce
selsabil-dev <boukaousselsabil00@gmail.com>
2025-11-14 22:42:23 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.write('Bienvenue sur mon serveur Node.js 🚀');
+  res.end();
+});
+
+server.listen(3000, () => {
+  console.log("Serveur démarré sur http://localhost:3000");
+});
